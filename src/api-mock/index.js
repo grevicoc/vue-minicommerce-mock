3afb769fbb6d03ce66efa60fe99e4ac04de90afe
grevicoc <cocabc1@gmail.com>
2021-09-14 17:52:19 +0700

    product details added

--- src/api-mock/index.js
+++ src/api-mock/index.js
@@ -40,18 +40,68 @@ const routes = [
     },
     {
         method: 'GET',
-        url: '/api/product/product-1',
+        url: '/api/products/product-1',
         status: 200,
         response: {
-          data: [
-            {
-              id: 'product-1',
-              image: 'https://via.placeholder.com/200',
-              title: 'Product 1',
-              price: 'Rp.100.000'
-            }
-          ]
+          data: {
+            id: 'product-1',
+            image: 'https://via.placeholder.com/200',
+            title: 'Product 1',
+            price: 'Rp.100.000'
+          }
         }
+    },
+    {
+      method: 'GET',
+      url: '/api/products/product-2',
+      status: 200,
+      response: {
+        data: {
+          id: 'product-2',
+          image: 'https://via.placeholder.com/200',
+          title: 'Product 2',
+          price: 'Rp.100.000'
+        }
+      }
+    },
+    {
+      method: 'GET',
+      url: '/api/products/product-3',
+      status: 200,
+      response: {
+        data: {
+          id: 'product-3',
+          image: 'https://via.placeholder.com/200',
+          title: 'Product 3',
+          price: 'Rp.100.000'
+        }
+      }
+    },
+    {
+      method: 'GET',
+      url: '/api/products/product-4',
+      status: 200,
+      response: {
+        data: {
+          id: 'product-4',
+          image: 'https://via.placeholder.com/200',
+          title: 'Product 4',
+          price: 'Rp.100.000'
+        }
+      }
+    },
+    {
+      method: 'GET',
+      url: '/api/products/product-5',
+      status: 200,
+      response: {
+        data: {
+          id: 'product-5',
+          image: 'https://via.placeholder.com/200',
+          title: 'Product 5',
+          price: 'Rp.100.000'
+        }
+      }
     }
   ]
   
--- src/store/index.js
+++ src/store/index.js
@@ -11,6 +11,13 @@ mock.onGet("/api/products").reply(200, {
   ...routeMock[0].response.data
 })
 
+for (let i=1; i<6; i++){
+  const mockdata = routeMock[i];
+  mock.onGet(mockdata.url).reply(mockdata.status,{
+    ...routeMock[i].response.data
+  })
+}
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
@@ -34,10 +41,16 @@ export default new Vuex.Store({
       axios.get("/api/products").then(function (response) {
         commit('setProducts', response.data)
       });
+    },
+    getProduct ({ commit }, param) {
+      axios.get("/api/products/" + param.id).then(function (response) {
+        commit('setProduct', response.data)
+      });
     }
   },
   getters: {
-    products: state => state.products
+    products: state => state.products,
+    product: state => state.product
   }
 
 })
